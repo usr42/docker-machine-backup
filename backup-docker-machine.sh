6be12aeae120264c28ec5789cc4fadf530ec6079
usr42 <usr42@users.noreply.github.com>
2018-11-23 16:48:51 +0100

    Initial version of backup and restore

---
+++ backup-docker-machine.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+usage() {
+    echo "usage: $0 MACHINE_NAME"
+    cleanup
+    exit 1
+}
+
+check_machine_exists() {
+    local NAME=$1
+
+    if [ ! -d "$HOME/.docker/machine/machines/$NAME" ]; then
+        echo "Docker machine \"$NAME\" does not exist. Use \"docker-machine ls\" to list machines."
+        usage
+    fi
+}
+
+cleanup() {
+    rm -rf $TMP_DIR
+}
+
+BASEDIR="$(pwd)"
+TMP_DIR=$(mktemp -d)
+cd $TMP_DIR
+
+if [ $# -lt 1 ]; then
+    usage
+fi
+
+MACHINE_NAME=$1
+
+check_machine_exists $MACHINE_NAME
+
+DEST_DIR=$TMP_DIR/$MACHINE_NAME
+MACHINE_DIR="$HOME/.docker/machine/machines/$MACHINE_NAME"
+CERTS_DIR="$HOME/.docker/machine/certs"
+OUTPUT_FILE=docker-machine-$MACHINE_NAME-backup.tar.gz
+
+TEMPLATE_MACHINE_NAME="___MACHINE_NAME___"
+TEMPLATE_HOME_DIR="___HOME_DIR___"
+
+mkdir $DEST_DIR
+cd $DEST_DIR
+cp $MACHINE_DIR/*.pem $MACHINE_DIR/config.json $MACHINE_DIR/id_rsa* $DEST_DIR
+cp $CERTS_DIR/*.pem $DEST_DIR
+
+sed -i.bak "s|$HOME|${TEMPLATE_HOME_DIR}|g" $DEST_DIR/config.json
+sed -i.bak "s|/$MACHINE_NAME/|/${TEMPLATE_MACHINE_NAME}/|g" $DEST_DIR/config.json
+sed -i.bak -e "s|\(\"Name\" *: *\"\)$MACHINE_NAME\"|\1${TEMPLATE_MACHINE_NAME}\"|g" $DEST_DIR/config.json
+sed -i.bak "s|.docker/machine/certs/|.docker/machine/machines/${TEMPLATE_MACHINE_NAME}/|g" $DEST_DIR/config.json
+
+cd $DEST_DIR
+tar czf $BASEDIR/$OUTPUT_FILE .
+echo Backup file created: $OUTPUT_FILE
+
+cleanup
---
+++ restore-docker-machine.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+usage() {
+    echo "usage: $0 MACHINE_NAME BACKUP_FILE"
+    exit 1
+}
+
+check_machine_exists() {
+    local NAME=$1
+
+    if [ -d "$HOME/.docker/machine/machines/$NAME" ]; then
+        echo "Docker machine with name \"$NAME\" does already exist. Use a different name."
+        usage
+    fi
+}
+
+
+if [ $# -lt 2 ]; then
+    usage
+fi
+
+MACHINE_NAME=$1
+BACKUP_FILE=$(pwd)/$2
+
+check_machine_exists $MACHINE_NAME
+if [ ! -f "$BACKUP_FILE" ]; then
+    echo "BACKUP_FILE \"$BACKUP_FILE\" does not exist."
+    usage
+fi
+
+MACHINE_DIR="$HOME/.docker/machine/machines/$MACHINE_NAME"
+TEMPLATE_MACHINE_NAME="___MACHINE_NAME___"
+TEMPLATE_HOME_DIR="___HOME_DIR___"
+
+mkdir $MACHINE_DIR
+tar xzf $BACKUP_FILE -C $MACHINE_DIR
+
+sed -i.bak1 "s|${TEMPLATE_HOME_DIR}|$HOME|g" $MACHINE_DIR/config.json
+sed -i.bak2 "s|\([/\"]\)$TEMPLATE_MACHINE_NAME\([/\"]\)|\1$MACHINE_NAME\2|g" $MACHINE_DIR/config.json
+
+echo "Successfully restored $2 as docker machine with name $MACHINE_NAME"
+
